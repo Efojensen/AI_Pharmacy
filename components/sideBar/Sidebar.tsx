dac1cc814c64a96df254f6df84fa5783c2a9b8ae
EfoJensen <Jensena231@gmail.com>
2025-10-31 11:14:22 +0000

    more sidebar components
    also some restructuring

--- components/sideBar/Sidebar.tsx
+++ components/sideBar/Sidebar.tsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
                     width={37}
                     height={35}
                     alt='company_logo'
-                    src='/svgs/mortar.svg'
+                    src='/svgs/sidebar/mortar.svg'
                 />
                 <h1 className='text-xl text-black font-normal'>
                     AI PHARMACY
@@ -28,6 +28,19 @@ const Sidebar = () => {
                 />
                 <SidebarTile icon='/svgs/sidebar/upload.svg' text='Upload' active={false} />
                 <SidebarTile icon='/svgs/sidebar/team.svg' text='Team' active={false} />
+            </nav>
+
+            <p className='text-sm leading-[1.125rem] font-normal ml-4 mb-[0.625rem] text-sideBarGrey mt-[1.5rem]'>Preferences</p>
+            <nav className='flex flex-col gap-y-[1.25rem] ml-1'>
+                <SidebarTile
+                    text='Settings'
+                    active={false}
+                    icon='/svgs/sidebar/settings.svg'
+                    dropdownArrow='/svgs/sidebar/dropdown.svg'
+                    dropdownItems={['Edit Profile', 'Privacy Policy', 'Notifications', 'Logout']}
+                />
+                <SidebarTile icon='/svgs/sidebar/about.svg' text='About' active={false} />
+                <SidebarTile icon='/svgs/sidebar/help.svg' text='Help' active={false} />
             </nav>
         </aside>
     )
